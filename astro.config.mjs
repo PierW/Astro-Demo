69491f4a4974ee1b90cc0ebf155559936a376582
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2024-04-05 17:55:18 +0200

    inserisco preact - island Astro per inserire del Js con framework e non

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://joyful-sorbet-36fb6e.netlify.app/"
+  site: "https://joyful-sorbet-36fb6e.netlify.app/",
+  integrations: [preact()]
 });
